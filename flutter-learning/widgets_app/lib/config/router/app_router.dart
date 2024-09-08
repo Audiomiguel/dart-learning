4137b5f29c720eee579f9b9784a6ef80df0320b2
Miguel Bustillos <miguel.bustillos@onroadts.com>
2024-09-08 18:31:20 -0500

    adding widgets app

---
+++ flutter-learning/widgets_app/lib/config/router/app_router.dart
@@ -0,0 +1,19 @@
+import 'package:go_router/go_router.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
+
+final appRouter = GoRouter(routes: [
+  GoRoute(
+      name: HomeScreen.name,
+      path: '/',
+      builder: (context, state) => const HomeScreen()),
+  GoRoute(
+      name: ButtonsScreen.name,
+      path: '/buttons',
+      builder: (context, state) => const ButtonsScreen()),
+  GoRoute(
+      name: CardScreen.name,
+      path: '/cards',
+      builder: (context, state) => const CardScreen())
+]);
---
+++ flutter-learning/widgets_app/lib/presentation/screens/cards/cards_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CardScreen extends StatelessWidget {
+  static const String name = 'cards_screen';
+
+  const CardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cards Screen'),
+      ),
+      body: Placeholder(),
+    );
+  }
+}
